5ebd9a7e091356d1b7dd6a7de54936ca743e004e
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-23 21:23:58 -0500

    fingers crossed

--- api/sendemail/index.js
+++ api/sendemail/index.js
@@ -1,5 +1,5 @@
 const nodemailer = require('nodemailer');
-const { SmsClient } = require('@azure/communication-sms');
+// const { SmsClient } = require('@azure/communication-sms');
 
 module.exports = async function (context, req) {
 //   const { DefaultAzureCredential } = require('@azure/identity');
@@ -21,15 +21,15 @@ module.exports = async function (context, req) {
     },
   });
 
-  const smsClient = new SmsClient('endpoint=https://kffsmscs.communication.azure.com/;accesskey=zM9uliUzojQ+GFjq6/Hewxe82vaLXsFPodOE3im6/wr1AggXC9+SiMYGufTOKM+bKDNYcj3OGXKYJ+FqdyewJA==');
-
+  // const smsClient = new SmsClient('endpoint=https://kffsmscs.communication.azure.com/;accesskey=zM9uliUzojQ+GFjq6/Hewxe82vaLXsFPodOE3im6/wr1AggXC9+SiMYGufTOKM+bKDNYcj3OGXKYJ+FqdyewJA==');
+  
   const mailOptions = {
     from: 'kaifamilyfarm123@outlook.com',
-    // to: 'kaifamilyfarm123@outlook.com',
     // from: 'kylehernandez12@outlook.com',
     // to: 'eands9@yahoo.com',
     // from: 'kaifamilyfarm@yahoo.com',
-    to: 'kaifamilyfarm@yahoo.com',
+    // to: 'kaifamilyfarm@yahoo.com',
+    to: 'kaifamilyfarm123@outlook.com',
     // from: 'kylehernandez12@outlook.com',
     // to: 'erichernandez134@outlook.com',
     subject:
@@ -43,23 +43,15 @@ module.exports = async function (context, req) {
       '</td></tr></tbody></table></div>',
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      smsClient.send({
-        from: "+18444021959",
-        to: ["+14699106366", "+14695250450"],
-        // to: ["+14699106366", "+14057610896"],
-        message: "Error in submission..."
-    });
-    } else {
-      console.log('Sent: ' + info.response);
-      smsClient.send({
-        from: "+18444021959",
-        // to: ["+14699106366", "+14695250450"],
-        to: ["+14699106366", "+14057610896"],
-        message: "Order from  " + req.body.emailAddress + "... " + req.body.orderTotal
-    });
-    }
-  });
-};
+  try {
+    await transporter.sendMail(mailOptions);
+    // console.log('Message sent: %s', response);
+          
+  } catch (error) {
+    console.log('****error =' + error);
+    context.res = {
+      status: 900,
+      // body: "Please pass a videoId on the query string or in the request body"
+    };
+  }
+}
